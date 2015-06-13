2446136e2d9efe76a1bbfc8adf0668354cd06536
Zoltan Orban <zoltan.orban@gmail.com>
2015-06-13 19:31:06 +0200

    V1.0

--- FrontEnd/js/map-controller.js
+++ FrontEnd/js/map-controller.js
@@ -1,88 +1,106 @@
 angular.module('appMaps', ['uiGmapgoogle-maps'])
 
-  .controller("mapCtrl", ["$scope", "$http", "uiGmapLogger", "uiGmapObjectIterators",
-    function ($scope,$http, logger, uiGmapObjectIterators) {
-      logger.doLog = true;
-      logger.currentLevel = logger.LEVELS.debug;
-      var lastId = 1;
-      var clusterThresh = 6;
-      $scope.showList = true;
-      $scope.actCanteen;
+.controller("mapCtrl", ["$scope", "$http", "uiGmapLogger", "uiGmapObjectIterators",
+  function ($scope,$http, logger, uiGmapObjectIterators) {
+    logger.doLog = true;
+    logger.currentLevel = logger.LEVELS.debug;
+    var lastId = 1;
+    var clusterThresh = 6;
+    $scope.showList = true;
+    $scope.actCanteen;
 
-      $scope.map = {
-        doCluster: true,
-        options: {
-          streetViewControl: false,
-          panControl: false,
-          maxZoom: 18,
-          minZoom: 3
-        },
-        events: {
-          idle: function () {
-            if ($scope.map.zoom <= clusterThresh) {
-              if (!$scope.map.doCluster) {
-                $scope.map.doCluster = true;
-                $scope.searchResults.results = [];
-              }
+    $scope.map = {
+      doCluster: true,
+      options: {
+        streetViewControl: false,
+        panControl: false,
+        maxZoom: 18,
+        minZoom: 3
+      },
+      events: {
+        idle: function () {
+          if ($scope.map.zoom <= clusterThresh) {
+            if (!$scope.map.doCluster) {
+              $scope.map.doCluster = true;
+              $scope.searchResults.results = [];
             }
-            else {
-              if ($scope.map.doCluster) {
-                $scope.map.doCluster = false;
-                $scope.searchResults.results = [];
-              }
-            }
-            $scope.addMarkers();
           }
-        },
-        center: {
-          latitude: 50.8434830,
-          longitude: 4.3825600
-        },
-        clusterOptions: {},
-        zoom: 15
-      };
-
-      $scope.searchResults = {
-        results: {
-          length: 0
+          else {
+            if ($scope.map.doCluster) {
+              $scope.map.doCluster = false;
+              $scope.searchResults.results = [];
+            }
+          }
+          $scope.addMarkers();
+        }, click: function(mapModel, eventName, originalEventArgs)
+        {
+          $scope.apply(function(){
+            alert('123');
+            var e = originalEventArgs[0];
+            var marker = {};
+            marker.coords = {};
+            marker.coords.latitude = e.latLng.lat();
+            marker.coords.longitude = e.latLng.lng();
+            $scope.map.markers.push(marker)
+          });
         }
-      };
+      },
+      eventsProperty : {
+        click : function(mapModel, eventName, originalEventArgs) {
+          console.log("user defined event on map directive with scope", this);
+          console.log("user defined event: " + eventName, mapModel, originalEventArgs);
+        }
+      },
+      center: {
+        latitude: 50.8434830,
+        longitude: 4.3825600
+      },
+      clusterOptions: {},
+      zoom: 15
+    };
+
+    $scope.searchResults = {
+      results: {
+        length: 0
+      }
+    };
 
 
-      $scope.addMarkers = function () {
-        var markers = {};
-        var i = 0;
+    $scope.addMarkers = function () {
+      var markers = {};
+      var i = 0;
         $http.get('json/canteens.json')//load model with delay
-    .success(function(data) {
+        .success(function(data) {
 
-        console.log(data);
-        $scope.canteenData = data;
-        for(i=0; i<data.Canteens.length;i++) {
-          markers[data.Canteens[i].CanteenID] ={
-            'coords': {
-              'latitude': data.Canteens[i].latitude,
-              'longitude': data.Canteens[i].longitude
-            },
-            'key': 'someKey-' + lastId
-          };
-          lastId++;
-        }
+          console.log(data);
+          $scope.canteenData = data;
+          for(i=0; i<data.Canteens.length;i++) {
+            markers[data.Canteens[i].CanteenID] ={
+              'coords': {
+                'latitude': data.Canteens[i].latitude,
+                'longitude': data.Canteens[i].longitude
+              },
+              'key': 'someKey-' + lastId,
+              'options': {title: data.Canteens[i].CanteenID, click: function(){alert('1')}}
+            };
+            lastId++;
+          }
         lastId = 1;//reset
         markers.length = data.Canteens.length;
         
         $scope.searchResults.results = uiGmapObjectIterators.slapAll(markers);
 
-    });         
+      });         
 
         $http.get('json/menu.json')//load model with delay
-    .success(function(data) {
+        .success(function(data) {
 
-        console.log(data);
-        $scope.menuData = data;
-    });         
+          console.log(data);
+          $scope.menuData = data;
+        });         
 
       };
-      
+
       $scope.reset = function () {
         lastId = 1;
         $scope.searchResults = {
@@ -105,4 +123,4 @@ angular.module('appMaps', ['uiGmapgoogle-maps'])
         }
       };
     }
-  ]);
+    ]);
